64b1a788d5ff47073ac19c8fa89fd275b6b55678
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-03-03 20:30:22 -0800

    cleaned up my mess

---
+++ ConsoleApplication1/ConsoleApplication1/Expression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Expression
+    {
+        public string ParseExpression(string equation)
+        {
+            string expression = 
+        }
+        
+
+//// Using Test Driven Development, create an Expression class that will break down a simple 2-term calculator expression.
+
+// Prove you can extract the terms of the expression.
+// Prove you can extract the operation embedded in the expression.
+// Ensure you have examples of GOOD and BAD input and have your Expression class throw an exception when there's an error.
+//Hints
+
+//Your expression should work with and without spaces. (i.e.both 2+1 and 2 + 1 should work)
+//The use of Regular Expressions is not required.
+//GOOD input example: 1+2
+//BAD input example +2
+//You do not need to handle the cases of usage of parenthesis or expressions with more than 2 terms.
+
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/ExpressionTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ExpressionTest
+    {
+        [TestMethod]
+        // Prove you can extract the terms of the expression.
+        public void MyTestMethod()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        // Prove you can extract the operation embedded in the expression.
+        public void MyTestMethod()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+        [TestMethod]
+        // Ensure you have examples of GOOD and BAD input and have your Expression class throw an exception when there's an error.
+        public void MyTestMethod()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        
+        
+       
+    }
+}
