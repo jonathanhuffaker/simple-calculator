3d0654d1cc0b6d1a5b54ff27991b44aadcfdf6ff
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-03-22 18:41:24 -0500

    started my Interface class for user entry since contstants were frustrating

---
+++ ConsoleApplication1/ConsoleApplication1/Interface.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Interface
+    {
+        public int counter {get; set;}
+
+        public Interface()
+        {
+            counter = 0;
+        }
+
+        public string FirstCalInput()
+        {
+            return String.Format("[{0}]>", counter);
+        }
+
+        public string AnsExpression(double answer)
+        {
+            string ansString = String.Format("  = {0}", answer);
+            counter++;
+            return ansString;
+        }
+
+        //do my last and lasq need to fit in here?
+    }
+
+
+}
